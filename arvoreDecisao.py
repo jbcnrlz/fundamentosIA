d9d24e7f76364686164902d46fb026a02ef90051
João Baptista Carida Neto <jbcnrlz@gmail.com>
2024-04-13 16:43:38 -0300

    Adicionando n

---
+++ arvoreDecisao.py
@@ -0,0 +1,25 @@
+import pandas as pd, numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+def loadDataset(pathDataset):
+    datasetCSV = pd.read_csv(pathDataset)
+    data = np.array(datasetCSV)
+    return data[:,:-1].astype(np.float64), data[:,-1], datasetCSV.columns
+
+def main():
+    data, classes, cols = loadDataset('datasets/iris.csv')
+    le = preprocessing.LabelEncoder()
+    yvs = le.fit_transform(classes)
+
+    X_train, X_test, y_train, y_test = train_test_split(data, yvs, random_state=0)
+
+    deClass = DecisionTreeClassifier(criterion='entropy')
+    deClass.fit(X_train,y_train)
+    guessed = deClass.predict(X_test)
+    print(classification_report(y_test, guessed))
+    
+if __name__ == '__main__':
+    main()
---
+++ trabalhoSala/1304_manha.py
@@ -0,0 +1,55 @@
+import pandas as pd, numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, cross_validate
+from sklearn.metrics import confusion_matrix
+
+def breastCancerEstimateDecisionTree():
+    arqCSV = pd.read_csv('datasets/winscosindata.csv')
+    dados = np.array(arqCSV)
+    caracteristicas = dados[:,2:-1].astype(np.float64)
+    classes = dados[:,1]
+
+    dTree = DecisionTreeClassifier(criterion='entropy')
+    mets = ['precision_macro','recall_macro','f1_macro','accuracy']
+    scores = cross_validate(dTree,caracteristicas,classes,cv=10,scoring=mets)
+    print("Métricas ========================")
+    for s in scores:
+        print("Média da %s => %f === desvio padrão %f" % (s,np.average(scores[s]),np.std(scores[s])))
+
+    predCross = cross_val_predict(dTree,caracteristicas,classes,cv=10)
+    confMatrix = confusion_matrix(classes,predCross)
+    print(confMatrix)
+
+
+def main():
+    arqCSV = pd.read_csv('datasets/iris.csv')
+    dados = np.array(arqCSV)
+    caracteristicas = dados[:,:-1].astype(np.float64)
+    classes = dados[:,-1]
+    dTree = DecisionTreeClassifier(criterion='entropy')
+
+    mets = ['precision_macro','recall_macro','f1_macro','accuracy']
+
+    scores = cross_validate(dTree,caracteristicas,classes,cv=10,scoring=mets)
+    print("Métricas ========================")
+    for s in scores:
+        print("Média da %s => %f === desvio padrão %f" % (s,np.average(scores[s]),np.std(scores[s])))
+
+    predCross = cross_val_predict(dTree,caracteristicas,classes,cv=10)
+    confMatrix = confusion_matrix(classes,predCross)
+    print(confMatrix)
+    #fTreino, fTeste, clasTreino, clasTeste = train_test_split(caracteristicas,classes)
+
+    
+    '''
+    dTree.fit(fTreino,clasTreino)
+
+    yPred = dTree.predict(fTeste)
+    desempenho = [0,0]
+    for i in range(len(yPred)):
+        desempenho[int(yPred[i] == clasTeste[i])] +=1
+
+    print("acertos: %d === erros %d" % (desempenho[1],desempenho[0]))
+    '''
+if __name__ == '__main__':
+    breastCancerEstimateDecisionTree()
